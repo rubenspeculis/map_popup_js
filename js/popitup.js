6806e07f7e280894ee956be738f08645cfd06327
Rubens Peculis <rubens.peculis@mdba.gov.au>
2013-09-19 14:07:00 +1000

    Added zoom to cluster

--- js/popitup.js
+++ js/popitup.js
@@ -8,7 +8,21 @@
 
     // Define mapPort and Append the new div
     var mapPort = $('.openlayers-views-map');
-    mapPort.append("<div id='map-info'></div>");
+    var topOffset = mapPort.find('#openlayers-container-openlayers-map').height();
+    var layers_html = $('#block-block-2');
+
+
+    mapPort.find('#openlayers-container-openlayers-map').append("<div id='toolset' style='margin-top:-"+topOffset+"px'></div>");
+
+    mapPort.find('#toolset').append("<div id='layers-selector' style='height:"+topOffset+"px;'><div id='full'></div><div id='layers'></div></</div>");
+    $('#layers').html(layers_html);
+    $('#block-block-2').css('display', 'block');
+    $('#block-block-2 .block-inner').hide();
+    $('.baseLbl').remove();
+    $('.baseLayersDiv').remove();
+    $('.dataLbl').remove();
+
+    mapPort.find('#toolset').append("<div id='map-info'></div>");
 
     // Hide Div by Default
     $('#map-info').hide()
@@ -31,13 +45,17 @@
           var featureDesc = '<div class="featureDesc">' + $('#popup_contentDiv').find('.openlayers-tooltip-description').html() + '</div>';
         }
         $('#map-info').html('<a href="#" class="toggleIt">X</a>').append(featureName).append(featureDesc);
-        $('#map-info').append('<a href="#" class="toggleIt">Close</a>');
         $('.toggleIt').on('click', function(event){
           event.preventDefault();
-          $('#map-info').hide('fast');
-          $('#map-info').html('');
+          $('#toolset').animate({width: '82px'}, 'fast', function(){
+            $('#map-info').hide();
+            $('#map-info').html('');
+          });
         });
-        $('#map-info').show('fast');
+        $('#toolset').animate({width: '432px'}, 'fast', function(){
+          $('#map-info').css('height', topOffset); 
+          $('#map-info').show();
+        });
       }
     });
 
@@ -55,28 +73,37 @@
         if($('#popup_contentDiv').is(':visible')){
           $('#popup_contentDiv').parent().parent().hide();
         }
-
-        var feature = '';
-        for(var i = 0; i < clusters.length; i++){
-          if( $(clusters[i]).find('.openlayers-tooltip-name').html() != null ){
-            feature += '<h3 class="featureName">' + $(clusters[i]).find('.openlayers-tooltip-name').html() + '</h3>';
-          }
-          if( $(clusters[i]).find('.openlayers-tooltip-description').html() != null ){
-            feature += '<div class="featureDesc">' + $(clusters[i]).find('.openlayers-tooltip-description').html() + '</div>';
-          }
-        }
-
-        $('#map-info').html('<a href="#" class="toggleIt">X</a>').append(feature);
-        $('#map-info').append('<a href="#" class="toggleIt">Close</a>');
-        $('.toggleIt').on('click', function(event){
-          event.preventDefault();
-          $('#map-info').hide('fast');
-          $('#map-info').html('');
-        });
-        $('#map-info').show('fast');
       }
+
+      var ol = $('.openlayers-map').data('openlayers').openlayers;
+      ol.zoomIn();
+
     });
+
+    $('#block-block-2').on('hover', function(event){
+      labelShower();
+    });
+
   });
+
+  function labelShower(){
+    $('#block-block-2 label').each(function(){
+        var flyoutText = ($(this).html());
+        var className = flyoutText.toLowerCase().replace(/ /g, '-');
+        var flyoutDiv = '<div class="flyout '+className+'">'+flyoutText+'</div>';
+        var countFlyOut = $('.'+className).length;
+        if(countFlyOut == 0 ){
+          $(this).after(flyoutDiv);
+        }
+        $('.flyout').hide();
+        $(this).on('mouseenter', function(){
+          $('.'+className).show();
+        });
+        $(this).on('mouseleave', function(){
+          $('.'+className).hide();
+        });
+    });
+  }
 
 
 })(jQuery);
